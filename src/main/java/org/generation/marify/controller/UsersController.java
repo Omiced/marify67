63c645da9a8d01d5eca5b179e20bc627ebffd311
Josue Cano <jdevelopcano@gmail.com>
2026-07-03 14:00:05 -0600

    finish users controller

--- src/main/java/org/generation/marify/controller/UsersController.java
+++ src/main/java/org/generation/marify/controller/UsersController.java
@@ -2,11 +2,9 @@ package org.generation.marify.controller;
 
 import lombok.AllArgsConstructor;
 import org.generation.marify.dto.UserResponse;
+import org.generation.marify.model.Users;
 import org.generation.marify.service.UsersService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,4 +23,24 @@ public class UsersController {
     public UserResponse getUserById(@PathVariable("userId") Long id){
         return usersService.getUserById(id);
     }
+
+    @PostMapping
+    public UserResponse addUser(@RequestBody Users user){
+        return usersService.createUser(user);
+    }
+
+    @PutMapping(path="{userId}")
+    public UserResponse updateUser(@PathVariable("userId") Long id, @RequestBody Users user){
+        return usersService.updateUserById(id, user);
+    }
+
+    @PostMapping(path = "/login")//http://localhost:8080/api/users/login
+    public Boolean loginUser(@RequestBody Users users){
+        return usersService.loginUser(users);
+    }
+    @DeleteMapping(path = "{userId}")
+    public UserResponse deleteUserById(@PathVariable("userId") Long id){
+        return usersService.deleteUserById(id);
+    }
+
 }
