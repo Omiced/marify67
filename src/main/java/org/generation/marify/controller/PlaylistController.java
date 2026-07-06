b5892a1033a52b81c49b8e905508800382640f93
Josue Cano <jdevelopcano@gmail.com>
2026-07-06 12:16:40 -0600

    finish playlist controller

--- src/main/java/org/generation/marify/controller/PlaylistController.java
+++ src/main/java/org/generation/marify/controller/PlaylistController.java
@@ -1,4 +1,31 @@
 package org.generation.marify.controller;
 
+import lombok.AllArgsConstructor;
+import org.generation.marify.dto.PlaylistRequest;
+import org.generation.marify.model.Playlist;
+import org.generation.marify.service.PlaylistService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path ="/api/playlist")
+@AllArgsConstructor
 public class PlaylistController {
+    private PlaylistService playlistService;
+
+    @GetMapping
+    public List<Playlist> getAllPlaylist(){
+        return playlistService.getAllPlaylists();
+    }
+
+    @GetMapping(path="{playlistId}")
+    public Playlist getPlaylistById(@PathVariable("playlistId") Long id ){
+        return playlistService.getSinglePlaylist(id);
+    }
+
+    @PostMapping
+    public Playlist addPlaylist(@RequestBody PlaylistRequest playlistRequest){
+        return playlistService.createPlaylist(playlistRequest);
+    }
 }
